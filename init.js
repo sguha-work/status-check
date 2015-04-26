e5f091132953e61cee29010976d5b8865d212103
Sahasrangshu Guha <sguha1988.life@gmail.com>
2015-04-26 13:15:30 +0530

    Delete init.js

--- init.js
+++
@@ -1,5 +0,0 @@
-var tl = require('./test-link-status');
-var fs = require('fs')
-tl.testLinkStatus("links.csv", function(data) {
-	fs.writeFile("output.json", JSON.stringify(data), function(){console.log("done")});
-});
