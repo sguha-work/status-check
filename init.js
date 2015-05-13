73d972cbe0eba44fd9d5ffd4aae8572d69a0a241
sguha-work <sguha1988.life@gmail.com>
2015-05-13 18:29:26 +0530

    changes

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 var sc = require('./status-check');
 var fs = require('fs');
-sc.testLinkStatus('links.csv', function(data) {
+sc.testLinkStatus('test.csv', function(data) {
 	fs.writeFile('output.json', JSON.stringify(data) , function(){
 		console.log("done");
 	});
